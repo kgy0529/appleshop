1b710040fbd7702c2a96c81ac3292f2782e64042
kgy0529 <rlarmsdud23@gmail.com>
2024-04-02 09:37:04 +0900

    first commit

---
+++ apple/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
+import './App.scss';
+import {Routes, Route} from 'react-router-dom';
+// import Main from "./pages/Main";
+import Store from "./pages/Store";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/store" element={<Store />}/>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
